986d092fef773974b076405a87db39f746c800d9
fly <albina.strecker@gmail.com>
2026-05-14 10:24:26 +0200

    feat: initialize empty game board

--- tictactoe/src/main/java/at/fhtechnikum/tictactoe/Board.java
+++ tictactoe/src/main/java/at/fhtechnikum/tictactoe/Board.java
@@ -1,4 +1,32 @@
 package at.fhtechnikum.tictactoe;
 
 public class Board {
+
+    private final char[][] cells;
+
+    public Board() {
+        cells = new char[3][3];
+        clearBoard();
+    }
+
+    public void clearBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                cells[row][col] = '-';
+            }
+        }
+    }
+
+    public char getCell(int row, int col) {
+        return cells[row][col];
+    }
+
+    public void printBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.print(cells[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
--- tictactoe/src/main/java/at/fhtechnikum/tictactoe/Main.java
+++ tictactoe/src/main/java/at/fhtechnikum/tictactoe/Main.java
@@ -3,6 +3,9 @@ package at.fhtechnikum.tictactoe;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("TicTacToe started");
+
+        Board board = new Board();
+
+        board.printBoard();
     }
 }
