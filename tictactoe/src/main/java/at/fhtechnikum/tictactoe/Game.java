7de25f79367f6edfe9a26b2cd623ec80d5098b01
electric sheep <if22b094@technikum-wien.at>
2026-06-21 05:11:53 +0200

    added display board feature

--- tictactoe/src/main/java/at/fhtechnikum/tictactoe/Game.java
+++ tictactoe/src/main/java/at/fhtechnikum/tictactoe/Game.java
@@ -35,6 +35,8 @@ public class Game {
             throw new IllegalArgumentException("Move is invalid or field is already occupied.");
         }
 
+        board.printBoard();  // Print board after valid move
+
         if (hasWinningCombination(currentPlayer.getMarker())) {
             winner = currentPlayer;
         }
